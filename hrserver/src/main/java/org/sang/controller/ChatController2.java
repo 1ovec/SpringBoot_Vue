dd38b84e98b3e1463e9b7d6a6281dbfe4f81480a
lichuang <101010.l>
2019-12-16 18:00:45 +0800

    增加一个没用的类

---
+++ hrserver/src/main/java/org/sang/controller/ChatController2.java
@@ -0,0 +1,25 @@
+package org.sang.controller;
+
+import org.sang.bean.Hr;
+import org.sang.bean.MsgContent;
+import org.sang.bean.RespBean;
+import org.sang.bean.SysMsg;
+import org.sang.service.HrService;
+import org.sang.service.SysMsgService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 处理通知消息的Controller
+ * 登录即可访问
+ */
+//@RestController
+//@RequestMapping("/chatC")
+public class ChatController2 {
+
+}
